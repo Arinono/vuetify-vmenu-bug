3ffb75102b44226d39a6a6c2586e26c6e6d57870
Aurelien Arino <aurelien@arino.io>
2024-07-30 11:59:45 +0200

    repro

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <HelloWorld />
+  <v-menu v-if="open" :close-on-content-click="false">
+    <template #activator="{ props }">
+      <v-btn v-bind="props">Open</v-btn>
+    </template>
+    <v-sheet class="pa-4">
+      <div class="d-flex">
+        <v-menu>
+          <template #activator="{ props }">
+            <v-btn style="display: block" v-bind="props">Sub menu</v-btn>
+          </template>
+          <v-sheet class="pa-4"> Click back in the input</v-sheet>
+        </v-menu>
+        <v-text-field style="width: 400px"></v-text-field>
+      </div>
+    </v-sheet>
+  </v-menu>
+
+  <router-link to="other-route" style="margin-left: 48px"> leave </router-link>
 </template>
 
 <script setup>
-  //
+import { ref } from "vue";
+
+const open = ref(true);
 </script>
---
+++ src/pages/other-route.vue
@@ -0,0 +1,9 @@
+<template>
+  other-route
+
+  <router-link to="/">go back</router-link>
+</template>
+
+<script setup>
+//
+</script>
